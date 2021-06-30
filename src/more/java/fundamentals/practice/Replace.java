169f3ae33d5192e3441d00e53495ac98c721c801
Kody Samaroo <github email address>
2021-06-30 10:47:26 -0400

    More Java fundamental exercise and practice.
    Replace practice done.

--- src/more/java/fundamentals/practice/Replace.java
+++ src/more/java/fundamentals/practice/Replace.java
@@ -4,14 +4,21 @@
  */
 package more.java.fundamentals.practice;
 
-
 public class Replace {
     
         public static void main(String[] args) {
             int first = 7;
-            int second = 19;  
+            int second = 19;
+            int third;
+            
+            third = second;
+            second = first;
+            first = third;
         
         System.out.println("first = " + first + " second = " +second);
+        // Created an empty third variable that would hold the value of second
+        // Then switched second to be first. first = 7, second = 7
+        // Used the third variable to make first equal 19 or what second was initally
     }
     
 }
