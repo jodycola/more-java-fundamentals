badc2c6644a47a39fee0c6ad0620745f86f90a81
Kody Samaroo <github email address>
2021-06-30 10:34:29 -0400

    More Java fundamental exercise and practice.

---
+++ src/more/java/fundamentals/practice/Replace.java
@@ -0,0 +1,17 @@
+/*
+ * Given two variables switch the variables so that first is 19 and second is 7.
+ * Dont use any more numbers
+ */
+package more.java.fundamentals.practice;
+
+
+public class Replace {
+    
+        public static void main(String[] args) {
+            int first = 7;
+            int second = 19;  
+        
+        System.out.println("first = " + first + " second = " +second);
+    }
+    
+}
